871ee083425cdc102a980dce1658a26d84a4c5d3
Chiwon <emphasize1@gmail.com>
2021-10-03 13:58:04 +0900

    style: correct directory names

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 
-import Layouts from './components/layouts';
-import Pages from './pages';
+import Layouts from './components/Layouts';
+import Pages from './Pages';
 
 const App: FC = () => {
   return (
